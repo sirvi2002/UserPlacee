fe504c27c02c3b0bfaef783e51f8081edd0d4b20
sirvi2002 <manoj260202@gmail.com>
2023-11-01 23:39:21 +0530

    Pushing into heroku with cors ( bcz in serve and backend folder its using prod webaddress)

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,12 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static(path.join('public')));
 
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With , Content-Type , Accept , Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-//     next();
-// })
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With , Content-Type , Accept , Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
